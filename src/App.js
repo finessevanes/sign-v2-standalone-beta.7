1e03f3c8c43e655978f7e1d2741dfe7f53f454b5
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-12-02 12:11:00 -0700

    4. Open QR Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { SignClient } from "@walletconnect/sign-client";
+import { Web3Modal } from "@web3modal/standalone";
 import "./App.css";
+
+const web3Modal = new Web3Modal({
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  standaloneChains: ["eip155:5"],
+});
 
 function App() {
   const [signClient, setSignClient] = useState();
@@ -16,6 +22,29 @@ function App() {
     }
   }
 
+  async function handleConnect() {
+    if (!signClient) throw Error("Client is not set");
+    try {
+      const proposalNamespace = {
+        eip155: {
+          methods: ["eth_sendTransaction"],
+          chains: ["eip155:5"],
+          events: ["connect", "disconnect"],
+        },
+      };
+
+      const { uri } = await signClient.connect({
+        requiredNamespaces: proposalNamespace,
+      });
+
+      if (uri) {
+        web3Modal.openModal({ uri });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   useEffect(() => {
     if (!signClient) {
       createClient();
@@ -25,6 +54,9 @@ function App() {
   return (
     <div className="App">
       <h1>Sign v2 Standalone</h1>
+      <button onClick={handleConnect} disabled={!signClient}>
+        Connect
+      </button>
     </div>
   );
 }
